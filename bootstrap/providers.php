ff6ff18660110729b7ab0770a201f6dd12c90a79
Mostafa Elbakly <elbakly@gmail.com>
2026-06-05 17:54:16 +0300

    fixing the class not found issue

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Providers\AppServiceProvider;
+use BaklySystems\AppWatch\AppswatchServiceProvider;
 
 return [
     AppServiceProvider::class,
+    AppswatchServiceProvider::class,
 ];
